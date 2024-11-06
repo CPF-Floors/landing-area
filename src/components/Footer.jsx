4439cfae2263c6edabcef46cfe9c37b4577dbe85
Alfredo <alfredo@cpffloors.com>
2024-11-06 15:33:52 -0400

    [dev] routes

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+
+
+function Footer() {
+  return (
+    <footer>
+        <div className="footer-card">
+            <img src="/landing-area/mail.svg" alt="" />
+            <h3>Contact Us</h3>
+            <p>Contact us for more quote</p>
+            <a href="mailto:contact@areacentre.com">contact@areacentre.com</a>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
