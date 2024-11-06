b7b41e4b37d55bd0c366d2f65791cc55d305b982
Alfredo <alfredo@cpffloors.com>
2024-11-06 15:29:38 -0400

    [dev] - footer update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
     <footer>
         <div className="footer-card">
             <img src="/mail.svg" alt="" />
-            <h3>E-mail</h3>
+            <h3>Contact Us</h3>
             <p>Contact us for more quote</p>
             <a href="mailto:contact@areacentre.com">contact@areacentre.com</a>
         </div>
