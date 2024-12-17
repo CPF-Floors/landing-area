e5ea161ec8405e9832f1c5681320615256b62756
Alfredo <offsound2020@gmail.com>
2024-12-17 12:08:26 -0400

    [dev] - styles update

--- src/components/Memberships.jsx
+++ src/components/Memberships.jsx
@@ -15,51 +15,63 @@ function Memberships() {
         </a>
         <div className="memberships-cards-container">
           <div className="membership-card">
-            <div className="membership-card-title">
-              <h2>PRIVATE OFFICE</h2>
-              <h4>Starting at $799</h4>
+            <div>
+              <div className="membership-card-title">
+                <h2>PRIVATE OFFICE</h2>
+                <h4>Starting at $799</h4>
+              </div>
+              <p>
+                Enjoy a fully equipped Private Office, perfect for individuals/
+                team looking to have their own space.
+              </p>
             </div>
-            <p>
-              Enjoy a fully equipped Private Office, perfect for individuals/
-              team looking to have their own space.
-            </p>
             <a href="#contact">EXPLORE</a>
           </div>
           <div className="membership-card">
-            <div className="membership-card-title">
-              <h2>DEDICATED DESK</h2>
-              <h4>Starting at $499</h4>
+            <div>
+              <div className="membership-card-title">
+                <h2>DEDICATED DESK</h2>
+                <h4>Starting at $499</h4>
+              </div>
+              <p>
+                A dedicated desk in a shared space gives you a personal
+                workstation with the chance to connect with a vibrant community
+                of professionals.
+              </p>
+              <span className="text-sart">
+                Note: For teams of two request for the dual-desk configuration
+              </span>
             </div>
-            <p>
-              A dedicated desk in a shared space gives you a personal
-              workstation with the chance to connect with a vibrant community of
-              professionals.
-            </p>
-            <span>
-              Note: For teams of two request for the dual-desk configuration
-            </span>
+
             <a href="#contact">EXPLORE</a>
           </div>
           <div className="membership-card">
-            <div className="membership-card-title">
-              <h2>FLEX SEAT</h2>
-              <h4>Starting at $399</h4>
+            <div>
+              <div className="membership-card-title">
+                <h2>FLEX SEAT</h2>
+                <h4>Starting at $399</h4>
+              </div>
+              <p>
+                With the Flex Seat membership, enjoy the freedom to choose a new
+                spot each day at our intimate work table, offering a
+                semi-private atmosphere ideal for freelancers or professionals
+                seeking a dynamic and flexible workspace.
+              </p>
             </div>
-            <p>
-            With the Flex Seat membership, enjoy the freedom to choose a new spot each day at our intimate work table, offering a semi-private atmosphere ideal for freelancers or professionals seeking a dynamic and flexible workspace.
-            </p>
             <a href="#contact">EXPLORE</a>
           </div>
           <div className="membership-card">
-            <div className="membership-card-title">
-              <h2>LOUNGE</h2>
-              <h4>Starting at $299</h4>
+            <div>
+              <div className="membership-card-title">
+                <h2>LOUNGE</h2>
+                <h4>Starting at $299</h4>
+              </div>
+              <p>
+                Access our cozy lounge area for casual work and networking. A
+                flexible option for those needing a professional space without a
+                fixed desk.
+              </p>
             </div>
-            <p>
-              Access our cozy lounge area for casual work and networking. A
-              flexible option for those needing a professional space without a
-              fixed desk.
-            </p>
             <a href="#contact">EXPLORE</a>
           </div>
         </div>
@@ -107,38 +119,40 @@ function Memberships() {
       <div id="about" className="includes-container-mobile">
         <h2>MEMBERSHIP INCLUDES</h2>
         <div className="includes-mobile">
-        <div className="includes-card-mobile">
-          <img src="/m1.svg" alt="" />
-          <p>Padel Courts</p>
-        </div>
-        <div className="includes-card-mobile">
-          <img src="/m4.svg" alt="" />
-          <p>Showers</p>
-        </div>
-        <div className="includes-card-mobile">
-          <img src="/m2.svg" alt="" />
-          <p>Phone Both</p>
-        </div>
-        <div className="includes-card-mobile">
-          <img src="/m5.svg" alt="" />
-          <p>Bathrooms</p>
-        </div>
-        <div className="includes-card-mobile">
-          <img src="/m6.svg" alt="" />
-          <p>Coffee</p>
-        </div>
-        <div className="includes-card-mobile">
-          <img src="/m7.svg" alt="" />
-          <p>Community</p>
-        </div>
-        <div className="includes-card-mobile">
-          <img src="/m8.svg" alt="" />
-          <p>Meeting <br /> Rooms</p>
-        </div>
-        <div className="includes-card-mobile">
-          <img src="/m3.svg" alt="" />
-          <p>Rest Room</p>
-        </div>
+          <div className="includes-card-mobile">
+            <img src="/m1.svg" alt="" />
+            <p>Padel Courts</p>
+          </div>
+          <div className="includes-card-mobile">
+            <img src="/m4.svg" alt="" />
+            <p>Showers</p>
+          </div>
+          <div className="includes-card-mobile">
+            <img src="/m2.svg" alt="" />
+            <p>Phone Both</p>
+          </div>
+          <div className="includes-card-mobile">
+            <img src="/m5.svg" alt="" />
+            <p>Bathrooms</p>
+          </div>
+          <div className="includes-card-mobile">
+            <img src="/m6.svg" alt="" />
+            <p>Coffee</p>
+          </div>
+          <div className="includes-card-mobile">
+            <img src="/m7.svg" alt="" />
+            <p>Community</p>
+          </div>
+          <div className="includes-card-mobile">
+            <img src="/m8.svg" alt="" />
+            <p>
+              Meeting <br /> Rooms
+            </p>
+          </div>
+          <div className="includes-card-mobile">
+            <img src="/m3.svg" alt="" />
+            <p>Rest Room</p>
+          </div>
         </div>
       </div>
     </>
